c1bc244ca19f4b3567696215d08e045f92230278
Miguel Nahum <miguel.nahum.olivares@gmail.com>
2026-05-14 22:45:43 -0400

    fix: agregar STATIC_ROOT al settings base para collectstatic en Railway

--- collico_sw/settings.py
+++ collico_sw/settings.py
@@ -138,7 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
